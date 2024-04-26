1c00b692ca523d54b43d10b80c4055fb3b218843
Aditya Khowal <108611217+AdityaKhowalGithub@users.noreply.github.com>
2024-04-25 21:45:45 -0700

    added terminal and fixed fuzzy finder outline

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,34 @@
+-- local builtin = require('telescope.builtin')
+-- vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+-- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+-- vim.keymap.set('n', '<leader>ps', function()
+	-- builtin.grep_string({ search = vim.fn.input("Grep > ") })
+-- end)
+--
+-- First, set up the Telescope with border and custom border characters
+require('telescope').setup{
+    defaults = {
+        border = true,
+        borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+        TelescopePreviewer = {
+            border = true,
+        },
+    }
+}
+
+-- Importing the builtin Telescope functions for easy access
 local builtin = require('telescope.builtin')
+
+-- Setting up key mappings for various Telescope functions
+-- Find files with a border
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+
+-- Find files in git with a border
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+
+-- Search for a string in your project with a border
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+
 

e917cad3c86d60f9e29e2b3d862855c2b6809a0d
Aditya Khowal <108611217+AdityaKhowalGithub@users.noreply.github.com>
2024-05-01 10:00:45 -0700

    trying out nvchad

---
+++ lua/chadrc.lua
@@ -0,0 +1,25 @@
+-- This file  needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
+
+---@type ChadrcConfig
+local M = {}
+
+M.ui = {
+	theme = "dark_horizon",
+  telescope = {style = "bordered"},
+  nvdash = {
+    load_on_startup = true,
+
+  }
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+-- M.plugins = {
+--   status{
+--     dashboard = true
+--   }
+-- }
+
+return M
